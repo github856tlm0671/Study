d1269ed06bca0a862ae7a19c4a270b2c6e1d382f
ddming <ddming0617@gmail.com>
2024-08-09 16:03:03 +0800

    数据结构和linux微项目

---
+++ program/test/main.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: ma6174
+    > Mail: ma6174@163.com
+    > Created Time: 2024年08月07日 星期三 21时12分20秒
+ ************************************************************************/
+
+#include "add.h"
+#include "sub.h"
+#include <stdio.h>
+
+int main() {
+
+  int a = 10;
+  int b = 5;
+  int res1 = add(a, b);
+  int res2 = sub(a, b);
+  printf("%d\n", res1);
+  printf("%d\n", res2);
+}
